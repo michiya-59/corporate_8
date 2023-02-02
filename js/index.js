5982a9f8cdc6f71bae6a2186bcb49197ece0c5f6
michiya-59 <nishino.michiya0509@gmail.com>
2023-02-03 07:33:31 +0900

    Galleryの完成

--- js/index.js
+++ js/index.js
@@ -37,11 +37,40 @@ function heade_fadein(scroll){
 
 function item_fadein(scroll, windowHeight){
   let site_title_height = $('.site_main_h1_text').offset().top;
+  let information_height = $('.information').offset().top;
+  let gallery_title = $('.gallery_title').offset().top;
 
+  // site_main_h1のフェードイン処理
   if (scroll > site_title_height - windowHeight - 50){
     $('.site_main_h1_text').addClass('site_main_h1_text_active');
   }
 
+  // informationのフェードイン処理
+  if (scroll > information_height - windowHeight){
+    $('.information').addClass('information_active');
+  }
+
+  // galleryのフェードイン処理
+  if (scroll > gallery_title - windowHeight){
+    $('.gallery_title').addClass('gallery_title_active');
+    $('.right_bottom_menu').addClass('right_bottom_menu_active');
+  } else{
+    $('.right_bottom_menu').removeClass('right_bottom_menu_active');
+  }
+
+  // 写真をループで回しているメソッド
+  picture_fadein(scroll, windowHeight, "picture")
+}
+
+// 写真をループで回しているメソッド
+function picture_fadein(scroll, windowHeight, class_name){
+  for (let i = 1; i < 6; i++) {
+    let element = $(`.${class_name}${i}`).offset().top;
+
+    if (scroll > element - windowHeight - 50){
+      $(`.${class_name}${i}`).addClass('picture_active');
+    }
+  }
 }
 
 function mv_scale(scroll){
